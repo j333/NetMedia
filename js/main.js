43f66f926764d49102dedab49e951c2172940439
Joel Gutiérrez <j333@live.com.ar>
2013-09-07 12:42:28 -0300

    - Corrección en menu - Incorporación de iconos en la esquina superior de cada sección. - Crearcion de Preloader

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+// PRELOADER
+$(window).load(function() { // makes sure the whole site is loaded
+	$("#status").fadeOut(); // will first fade out the loading animation
+	$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+})
+// ASCENSOR
 $('#ascensor').ascensor();
-var menuLeft = document.getElementById( 'cbp-spmenu-s' ),
-                body = document.body;
-
-            showMenu.onclick = function() {
-                classie.toggle( menuLeft, 'cbp-spmenu-open' );
-            };
-            ascensor.onclick = function() {
-                classie.remove( menuLeft, 'cbp-spmenu-open' );
-            };
+	var menuLeft = document.getElementById( 'cbp-spmenu-s' ),
+        body = document.body;
+// MENU
+showMenu.onclick = function() {
+    classie.toggle( menuLeft, 'cbp-spmenu-open' );
+};
+ascensor.onclick = function() {
+    classie.remove( menuLeft, 'cbp-spmenu-open' );
+};
