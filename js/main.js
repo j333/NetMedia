d77021661a4b29f63082e120354850fcef0a1196
Joel Gutiérrez <j333@live.com.ar>
2013-09-19 18:40:56 +0200

    Scroll en loading
    Supresión de la barra de scroll en la verntana de loading.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 $(window).load(function() { // makes sure the whole site is loaded
 	$("#status").fadeOut(); // will first fade out the loading animation
 	$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+    $("html").css({ 'overflow-y' : 'auto'});
+    $("body").css({ 'overflow-y' : 'auto'});
 })
 
 // ASCENSOR
